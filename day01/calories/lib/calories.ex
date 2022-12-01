7a2132e882f833c9334bd4d1b3dab14e08f0fa32
kintrix007 <boldizsar02@gmail.com>
2022-12-01 13:17:02 +0100

    Cleaned it up a bit

--- day01/calories/lib/calories.ex
+++ day01/calories/lib/calories.ex
@@ -2,8 +2,8 @@ defmodule Calories do
   #? First time writing Elixir
   #? I would assume this is messier than it needs to be
 
-  def part1() do
-    {:ok, content} = File.read("input")
+  def parse(file) do
+    {:ok, content} = File.read(file)
 
     dwarfs = String.split(content, "\n\n", trim: true)
     |> Enum.map(fn x ->
@@ -11,23 +11,19 @@ defmodule Calories do
       |> Enum.map(&Integer.parse/1)
       |> Enum.map(fn {x, _} -> x end)
     end)
-    |> Enum.map(&Enum.sum/1)
 
-    Enum.max dwarfs
+    dwarfs
+  end
+
+  def part1() do
+    parse("input")
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.max
   end
 
   def part2() do
-    {:ok, content} = File.read("input")
-
-    dwarfs = String.split(content, "\n\n", trim: true)
-    |> Enum.map(fn x ->
-      String.split(x, "\n", trim: true)
-      |> Enum.map(&Integer.parse/1)
-      |> Enum.map(fn {x, _} -> x end)
-    end)
+    parse("input")
     |> Enum.map(&Enum.sum/1)
-
-    dwarfs
     |> Enum.sort_by(&-/1) # Sort by negated elements. e.g. descending order
     |> Enum.take(3)
     |> Enum.sum
