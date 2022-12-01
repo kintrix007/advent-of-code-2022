7a45b9a68ded3ca44cb062cc95d27d5be4802b90
kintrix007 <boldizsar02@gmail.com>
2022-12-01 10:36:41 +0100

    Solved day 1 part 2
    Also found out about function references

--- day01/calories/lib/calories.ex
+++ day01/calories/lib/calories.ex
@@ -1,14 +1,34 @@
 defmodule Calories do
+  #? First time writing Elixir
+  #? I would assume this is messier than it needs to be
+
   def part1() do
     {:ok, content} = File.read("input")
+
     dwarfs = String.split(content, "\n\n", trim: true)
     |> Enum.map(fn x ->
       String.split(x, "\n", trim: true)
-      |> Enum.map(fn str -> Integer.parse str end)
+      |> Enum.map(&Integer.parse/1)
       |> Enum.map(fn {x, _} -> x end)
     end)
-    |> Enum.map(fn x -> Enum.sum(x) end)
+    |> Enum.map(&Enum.sum/1)
 
     Enum.max dwarfs
+  end
+
+  def part2() do
+    {:ok, content} = File.read("input")
+    
+    dwarfs = String.split(content, "\n\n", trim: true)
+    |> Enum.map(fn x ->
+      String.split(x, "\n", trim: true)
+      |> Enum.map(&Integer.parse/1)
+      |> Enum.map(fn {x, _} -> x end)
+    end)
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.sort_by(fn x -> -x end)
+
+    [d1 | [ d2 | [ d3 | _ ]]] = dwarfs
+    Enum.sum [ d1, d2, d3 ]
   end
 end
