f02cda9b42f42869ea4af1ce7b6d9c13570a73e9
kintrix007 <boldizsar02@gmail.com>
2022-12-02 14:38:06 +0100

    Can do it more efficiently by using streams..?

--- day01/calories/lib/calories.ex
+++ day01/calories/lib/calories.ex
@@ -6,10 +6,10 @@ defmodule Calories do
     {:ok, content} = File.read(file)
 
     dwarfs = String.split(content, "\n\n", trim: true)
-    |> Enum.map(fn x ->
+    |> Stream.map(fn x ->
       String.split(x, "\n", trim: true)
-      |> Enum.map(&Integer.parse/1)
-      |> Enum.map(fn {x, _} -> x end)
+      |> Stream.map(&Integer.parse/1)
+      |> Stream.map(fn {x, _} -> x end)
     end)
 
     dwarfs
@@ -17,15 +17,15 @@ defmodule Calories do
 
   def part1() do
     parse("input")
-    |> Enum.map(&Enum.sum/1)
+    |> Stream.map(&Enum.sum/1)
     |> Enum.max
   end
 
   def part2() do
     parse("input")
-    |> Enum.map(&Enum.sum/1)
+    |> Stream.map(&Enum.sum/1)
     |> Enum.sort_by(&-/1) # Sort by negated elements. e.g. descending order
-    |> Enum.take(3)
+    |> Stream.take(3)
     |> Enum.sum
   end
 end
