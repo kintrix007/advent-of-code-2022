9d521e829adee3417ae045625176be53861decd3
kintrix007 <boldizsar02@gmail.com>
2022-12-07 15:27:01 +0100

    Solved day 7 part 1

--- day07/part1.sh
+++ day07/part1.sh
@@ -4,21 +4,24 @@ DU=`du -b fs/*`
 
 SUM=0
 
+function get-actual() {
+	local path=$1
+	local size=$2
+
+	local subdirs=`echo "$DU" | grep "$FILEPATH" | wc -l`
+	let subdirs--
+
+	let size-=$subdirs*4096
+	if [[ -d $path ]]; then
+		let size-=4096
+	fi
+	echo $size
+}
+
 while read l; do
 	FILEPATH=`cut -f 2 <<< $l`
 	SIZE=`cut -f 1 <<< $l`
-
-	SUBDIRS=`echo "$DU" | grep "$FILEPATH" | wc -l`
-	let SUBDIRS--
-	#echo -n "$FILEPATH has $SUBDIRS subdirs"
-
-	filesize=$SIZE
-	let filesize-=$SUBDIRS*4096
-	if [[ -d $FILEPATH ]]; then
-		let filesize-=4096
-		#echo -n " and is a dir"
-	fi
-	echo
+	filesize=`get-actual $FILEPATH $SIZE`
 
 	if [[ $filesize -le 100000 ]]; then
 		let SUM+=$filesize
