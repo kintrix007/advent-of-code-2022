1d8ba5ec1488a62f8852f5fc35149cb4e24ef527
kintrix007 <boldizsar02@gmail.com>
2022-12-09 20:04:32 +0100

    Solved day 9 part 1

---
+++ day09/src/day09.nim
@@ -0,0 +1,70 @@
+import strutils
+import strformat
+import sequtils
+import fusion/matching
+import std/sets
+# import sugar
+
+type
+  Coord = tuple[x: int, y: int]
+  Parsed = seq[tuple[dir: Dir, amount: int]]
+  Dir = enum
+    left, right, up, down
+
+func parseDir(str: string): Dir
+func dist(p1, p2: Coord): int
+
+proc part1(parsed: Parsed): int =
+  var 
+    h = (0, 0).Coord
+    t = (0, 0).Coord
+    visited = toHashSet [t]
+  
+  #? Those parentheses DO make a difference
+  for (dir, amount) in parsed:
+    for _ in 0..<amount:
+      let prev = h
+      case dir:
+        of up:    inc h.y
+        of down:  dec h.y
+        of right: inc h.x
+        of left:  dec h.x
+      
+      let d = h.dist(t)
+      
+      if d > 1:
+        t = prev
+        visited.incl t
+      
+      echo fmt"H: {h:20}T: {t:20}dist: {d}"
+
+  visited.len
+
+## Weird implementation of dist for this problem
+func dist(p1, p2: Coord): int =
+  max(abs(p1.x-p2.x), abs(p1.y-p2.y))
+
+proc parse(filename: string): Parsed =
+  let cont = filename.readFile().strip()
+  cont.splitLines().map do (x: string) -> auto:
+    [@dirStr, @amountStr] := x.split(" ")
+    let
+      dir = dirStr.parseDir()
+      amount = amountStr.parseInt()
+    (dir: dir, amount: amount)
+
+func parseDir(str: string): Dir =
+  case str:
+    of "L": left
+    of "R": right
+    of "U": up
+    of "D": down
+    else: raise newException(ValueError, "Wrong dir '%s'" % [str])
+
+when isMainModule:
+  let parsed = parse("input")
+  # echo parsed
+  let
+    p1 = part1 parsed
+  
+  echo fmt"Part1: {p1}"
